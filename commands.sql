8f606130a2e6992b8703219f3be14fd057449371
DylanG129 <123974209+DylanG129@users.noreply.github.com>
2023-10-30 13:10:11 -0400

    Command context
    added comments to commands

--- commands.sql
+++ commands.sql
@@ -1,18 +1,25 @@
+-- adds new entry
 INSERT INTO Passwords (WebsiteName, WebsiteURL, FirstName, LastName, Username, Email, Password, Comment)
 VALUES ('Mysql', 'http://www.mysql.com', 'Dylan', 'Gomez', 'DylanG123', 'Dgomez123@yahoo.com', AES_ENCRYPT('RedCat14', @key_str, @init_vector), 'New MySQL account');
 
+-- gives back deciphered password
 SELECT CAST(AES_DECRYPT(Password, @key_str, @init_vector) AS CHAR) AS Real_pass FROM Passwords WHERE WebsiteURL = 'http://www.someweb.com';
 
+-- gives back all entry info from a limit of WebsiteURLs with 'https'
 SELECT *, CAST(AES_DECRYPT(Password, @key_str, @init_vector)AS CHAR) AS Real_pass
 FROM Passwords 
 WHERE WebsiteURL LIKE 'https://%' 
 LIMIT 2;
 
+-- change WebsiteURL using WebsiteURL
 UPDATE Passwords SET WebsiteURL = 'http://www.newsite.com' WHERE WebsiteURL = 'https://www.actualsite.com';
 
+-- change Password using WebsiteURL
 UPDATE Passwords SET Password = AES_ENCRYPT('newpassword', @key_str, @init_vector) WHERE WebsiteURL = 'https://www.somesite.com';
 
+-- delete entry based on given WebsiteURL
 DELETE FROM Passwords WHERE WebsiteURL = 'http://www.thesite.com';
 
+-- delete entry based on given Password
 DELETE FROM Passwords WHERE AES_DECRYPT(Password, @key_str, @init_vector) = 'thepass';
 
